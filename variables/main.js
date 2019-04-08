73af1cddfa851640db2d95813dba62d94c77f064
Dmitry Kharlamov <majsberg@yandex.ru>
2019-04-08 20:04:13 +0300

    Вторая задача

--- variables/main.js
+++ variables/main.js
@@ -1,25 +1,19 @@
 "use strict";
-function getResult(a, b, c) {
-  let x1;
-  let x2;
-  let m = [];
-  let result;
-  let d = Math.pow(b, 2) - 4 * a * c;
-  if (d > 0) {
-    x1 = (-b + Math.sqrt(d)) / (2 * a);
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
-    m = [x1, x2];
-    result = `Корни уравнения: ${m}`;
-    return result;
-  } else if (d == 0) {
-    x1 = -b / (2 * a);
-    result = `Корень уравнения: ${x1}`;
-    return result;
+
+function askDrink(name, dateOfBirthday) {
+  let birthday = new Date(dateOfBirthday);
+  let now = new Date();
+  let nowYear = now.getFullYear();
+  let birthdayYear = birthday.getFullYear();
+  let old = nowYear - birthdayYear;
+  if (old >= 18) {
+    let firstPhrase = `Не желаете ли олд-фэшн, ${name}?`
+    return firstPhrase;
   } else {
-    result = "Уравнение не имеет корней";
-    return result;
+    let secondPhrase = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+    return secondPhrase;
   }
 }
 
-let intrest = getResult(3, -14, -5);
-console.log(intrest);
+let question = askDrink("Марк","1988-10-22");
+console.log(question);
