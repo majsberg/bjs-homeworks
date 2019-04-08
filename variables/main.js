4f88a0797473589e63e3bfb352afc97858f06f5f
Dmitry Kharlamov <majsberg@yandex.ru>
2019-04-08 20:10:56 +0300

    Третья задача

--- variables/main.js
+++ variables/main.js
@@ -1,19 +1,19 @@
 "use strict";
 
-function askDrink(name, dateOfBirthday) {
-  let birthday = new Date(dateOfBirthday);
-  let now = new Date();
-  let nowYear = now.getFullYear();
-  let birthdayYear = birthday.getFullYear();
-  let old = nowYear - birthdayYear;
-  if (old >= 18) {
-    let firstPhrase = `Не желаете ли олд-фэшн, ${name}?`
-    return firstPhrase;
-  } else {
-    let secondPhrase = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
-    return secondPhrase;
+alert('После каждой оценки нужно вводить запятую и пробел. \nУчтите, что средняя оценка выставляется только для пяти оценок и менее');
+function getAverageMark(marks) {
+  marks = prompt('Введите оценки ученика');
+  let arr = marks.split(', ', 5);
+  /*console.log(arr[0]);
+  console.log(arr[1]);
+  console.log(arr[2]);*/
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + +arr[i];
   }
+  /*console.log(sum);*/
+  let average = sum / arr.length;
+  return  `Средняя отметка: ${average}`;
 }
-
-let question = askDrink("Марк","1988-10-22");
-console.log(question);
+let message = getAverageMark();
+alert(message);
